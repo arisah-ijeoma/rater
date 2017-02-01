2df756478bc3508d9d044e94c6074cdfe0f57f09
Ijeoma Arisah <ijeomarisah@gmail.com>
2017-02-01 17:37:12 +0100

    validate sign up fields

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,9 @@ class User < ActiveRecord::Base
   NIGERIAN_STATES = @@states_and_capitals.map { |h| h.slice(:name).values }
 
   ACCOUNT_TYPES = %w( Student Lecturer Politician Pastor )
+
+  validates :phone_number, :state_of_origin, :state_of_residence, presence: true
+  validates :email, format: { with: Devise.email_regexp }
+  validates :phone_number, format: { with: /[[:digit:]]{10}/ }, length: {minimum: 11, maximum: 14}
+
 end
