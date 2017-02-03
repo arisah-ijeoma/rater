71294e480b5dcd8fb91f1969f37a1397e2d67374
Ijeoma Arisah <ijeomarisah@gmail.com>
2017-02-03 17:43:42 +0100

    fix sign in page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,15 @@
 module ApplicationHelper
   def alert_color(name)
     case name
-      when "notice" then "success"
-      when "alert" then "danger"
+      when 'notice' then 'success'
+      when 'alert' then 'danger'
       else name
     end
+  end
+
+  def show_devise(session, reg, psw)
+    controller_name == session ||
+    controller_name == reg ||
+    controller_name == psw
   end
 end
