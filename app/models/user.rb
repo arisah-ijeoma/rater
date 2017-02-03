1c58a04c437a2476dc36fa27201e4ce2781ee56b
Ijeoma Arisah <ijeomarisah@gmail.com>
2017-02-03 01:58:17 +0100

    add anonymous to ui account type dropdown

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,7 @@ class User < ActiveRecord::Base
   @@states_and_capitals = LocationsNg::State.all
   NIGERIAN_STATES = @@states_and_capitals.map { |h| h.slice(:name).values }
 
-  ACCOUNT_TYPES = %w( Student Lecturer Politician Pastor )
+  ACCOUNT_TYPES = %w( Anonymous Student Lecturer Politician Pastor )
 
   validates :phone_number, :state_of_origin, :state_of_residence, presence: true
   validates :email, format: { with: Devise.email_regexp }
