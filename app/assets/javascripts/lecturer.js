986840d0008e19bdf20eef8b00bb558de063e338
Ijeoma Arisah <ijeomarisah@gmail.com>
2017-04-14 22:44:47 +0100

    user can add extra comment for lecturer

--- app/assets/javascripts/lecturer.js
+++ app/assets/javascripts/lecturer.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
   $('.lecturers').hide();
+  $('.lecturer-comments').hide();
 
   $('.view-lecturers').on('click', function() {
     $('.lecturers').show();
@@ -9,5 +10,19 @@ $(document).ready(function() {
   $('.hide-lecturers').on('click', function() {
     $('.lecturers').hide();
     $('.view-lecturers').show();
+  });
+
+  $('#lecturer_comments').on('click', function() {
+    $('.lecturers').hide();
+    $('.lecturer-comments').show();
+    $('.hide-lecturers').hide();
+  });
+
+  $('.hide-lecturer-comments').on('click', function() {
+    $('.lecturers').show();
+    $('.view-lecturers').hide();
+    $('.school-comments').hide();
+    $('.lecturer-comments').hide();
+    $('.hide-lecturers').show();
   })
 });
